41ddd614624b30e26562c82510becd4b5ed75b4a
alanionita <a.g.ionita@gmail.com>
2017-11-26 14:05:50 +0000

    [redux/actions] added first async action for fetching photos + test

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,21 +1,39 @@
 import * as types from './types';
+import axios from 'axios';
+
+const API_URL =
+  'https://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=nameOfCallback&tags=goats';
 
 export function fetchPhotosRequest () {
-    return {
-        type: types.FETCH_PHOTOS_REQUEST
-    };
+  return {
+    type: types.FETCH_PHOTOS_REQUEST
+  };
 }
 
 export function fetchPhotosSuccess (photos) {
-    return {
-        type: types.FETCH_PHOTOS_SUCCESS,
-        payload: photos
-    };
+  return {
+    type: types.FETCH_PHOTOS_SUCCESS,
+    payload: photos
+  };
 }
 
 export function fetchPhotosFailed (err) {
-    return {
-        type: types.FETCH_PHOTOS_FAILED,
-        payload: err
-    };
+  return {
+    type: types.FETCH_PHOTOS_FAILED,
+    payload: err
+  };
+}
+
+export function fetchPhotos () {
+  return function (dispatch) {
+    dispatch(fetchPhotosRequest());
+    return axios
+      .get(`${API_URL}`)
+      .then(res => {
+        dispatch(fetchPhotosSuccess(res.data));
+      })
+      .catch(err => {
+        dispatch(fetchPhotosFailed(err));
+      });
+  };
 }
--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,4 +1,12 @@
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import moxios from 'moxios';
+
 import * as actions from './actions';
+import * as types from './types';
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
 
 describe('PHOTOS ACTIONS', () => {
   describe('fetchPhotosRequest', () => {
@@ -32,6 +40,58 @@ describe('PHOTOS ACTIONS', () => {
       expect(actions.fetchPhotosFailed(err)).toEqual({
         type: 'FETCH PHOTOS FAILED',
         payload: err
+      });
+    });
+  });
+  describe('fetchPhotos ASYNC', () => {
+    beforeEach(() => {
+      moxios.install();
+    });
+
+    afterEach(() => {
+      moxios.uninstall;
+    });
+    test('returns correct series of actions and payload if succesful', () => {
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 200,
+          response: {
+            items: [
+                { 
+                    title:'Stretching for food',
+                    link: 'https://www.flickr.com/photos/vassilisonline/38593667296/'
+                }
+            ]
+          }
+        });
+      });
+
+      const store = mockStore({
+        items: [
+            { 
+                title:'Stretching for food',
+                link: 'https://www.flickr.com/photos/vassilisonline/38593667296/'
+            }
+        ]
+      });
+
+      const expectedActions = [
+        { type: types.FETCH_PHOTOS_REQUEST },
+        {
+          type: types.FETCH_PHOTOS_SUCCESS,
+          payload: {
+            items: [
+                { 
+                    title:'Stretching for food',
+                    link: 'https://www.flickr.com/photos/vassilisonline/38593667296/'
+                }
+            ]
+          }
+        }
+      ];
+      return store.dispatch(actions.fetchPhotos()).then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
       });
     });
   });
