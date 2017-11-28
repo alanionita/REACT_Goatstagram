2b6687162e12a60cd0d4847d5ea478c188d1b0ec
alanionita <a.g.ionita@gmail.com>
2017-11-28 10:36:01 +0000

    updated styling to Header and Nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
-const Header = () => (<header className="level is-mobile">
-      <h1 className="level-item is-size-3">
-        Goatstagram{' '}
-        <span role="img" aria-label="goat emoji">
-          🐐
-        </span>
-      </h1>
-    </header>);
+const Header = () => (
+  <header className="hero is-primary">
+    <div className="hero-body">
+        <div className="container">
+          <h1 className="title has-text-centered is-size-3-touch is-size-3-desktop is-size-3-widescreen is-size-3-fullhd">
+            Goatstagram{' '}
+            <span role="img" aria-label="goat emoji">
+              🐐
+            </span>
+          </h1>
+          <h2 className="subtitle has-text-centered is-size-6-touch is-size-6-desktop is-size-6-widescreen is-size-6-fullhd">
+            The social network for goat-tastic pictures
+          </h2>
+        </div>
+      </div>
+  </header>
+);
 
 export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => (
   <nav className="tabs is-centered">
     <ul>
       <li>
-        <a href="/all">Browse all</a>
+        <Link to="/all">Browse all</Link>
       </li>
       <li>
-        <a href="/favourites">Favourites</a>
+        <Link to="/favourites">Favourites</Link>
       </li>
     </ul>
   </nav>
